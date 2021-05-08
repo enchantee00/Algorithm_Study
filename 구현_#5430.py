1acf9ac5031726b8a3c280e9deed22d498f6005e
Yoon <62553866+Jiyoonjung28@users.noreply.github.com>
2021-05-08 21:08:34 +0900

    Update 구현_#5430.py

--- 구현_#5430.py
+++ 구현_#5430.py
@@ -6,7 +6,7 @@ def func(array_chunk, array):
     R_num= 0
     for i in array_chunk:
         if 'R' in i:
-            R_num += len(i)
+            R_num += len(i)     #매번 뒤집지 않아도 된다. -> 시간 복잡도 O(N)
         else:
             if len(array) >= len(i):
                 cut= len(i)
@@ -19,7 +19,7 @@ def func(array_chunk, array):
                 return
 
     if R_num % 2 == 0:
-        ans= ''.join(str(array).split())
+        ans= ''.join(str(array).split())    #리스트 출력할 때, 공백 없이 하기 위해서 string으로 바꿔준다.
         print(ans)
     else:
         array.reverse()
@@ -29,7 +29,7 @@ def func(array_chunk, array):
 
 T= int(sys.stdin.readline())
 for i in range(T):
-    p= sys.stdin.readline().strip('\n')
+    p= sys.stdin.readline().strip('\n')     #항상 sys.stdin.readline()에는 '\n'이 뒤에 붙는다.
     n= int(sys.stdin.readline())
     s= sys.stdin.readline().strip('\n')
     if len(s) == 2:
