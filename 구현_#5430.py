71f173a86aa5518680bf53b6bdec3b8cd5090e21
Yoon <62553866+Jiyoonjung28@users.noreply.github.com>
2021-05-08 21:04:06 +0900

    Add files via upload

---
+++ 구현_#5430.py
@@ -0,0 +1,40 @@
+import sys
+from itertools import groupby
+
+
+def func(array_chunk, array):
+    R_num= 0
+    for i in array_chunk:
+        if 'R' in i:
+            R_num += len(i)
+        else:
+            if len(array) >= len(i):
+                cut= len(i)
+                if R_num % 2 == 0:
+                    del array[0:cut]
+                else:
+                    del array[len(array) - cut:]
+            else:
+                print('error')
+                return
+                
+    if R_num % 2 == 0:
+        print(array)
+    else:
+        array.reverse()
+        print(array)
+
+
+T= int(sys.stdin.readline())
+for i in range(T):
+    p= sys.stdin.readline().strip('\n')
+    n= int(sys.stdin.readline())
+    s= sys.stdin.readline().strip('\n')
+    if len(s) == 2:
+        array= []
+    else:
+        array= list(map(int, s.strip('[]\n').split(',')))
+    array_chunk= [''.join(g) for _, g in groupby(p)]
+
+    func(array_chunk, array)
+
