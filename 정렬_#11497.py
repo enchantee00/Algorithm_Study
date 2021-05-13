c923cb90b74ddf3d125ebe7a0685aab34cbd134e
Yoon <62553866+Jiyoonjung28@users.noreply.github.com>
2021-05-14 01:28:17 +0900

    Add files via upload

---
+++ 정렬_#11497.py
@@ -0,0 +1,37 @@
+import sys
+
+
+
+def least_diff(array):
+    global ans_log
+    if len(array) % 2 == 0:
+        for i in range(N // 2):
+            ans_log.append(array[2*i])
+        array.reverse()
+        for s in array:
+            ans_log.append(s)
+    else:
+        for i in range(N // 2 + 1):
+            ans_log.append(array[2*i])
+        array.reverse()
+        for s in array:
+            ans_log.append(s)
+
+
+
+T = int(sys.stdin.readline())
+for i in range(T):
+    N = int(sys.stdin.readline())
+    log = list(map(int, sys.stdin.readline().split()))
+    sort_log = sorted(log)
+    ans_log = []
+    least_diff(sort_log)
+    max_value = 0
+    for i in range(N):
+        check = abs(ans_log[i] - ans_log[i-1])
+        if check > max_value:
+            max_value = check
+    print(max_value)
+
+
+    
