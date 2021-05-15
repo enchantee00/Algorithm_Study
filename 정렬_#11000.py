11c648afcc5688cc064c6380428edf2bbbfc6549
Yoon <62553866+Jiyoonjung28@users.noreply.github.com>
2021-05-15 12:24:43 +0900

    Add files via upload

---
+++ 정렬_#11000.py
@@ -0,0 +1,17 @@
+import heapq
+import sys
+
+N = int(sys.stdin.readline())
+C = []
+h = []
+for i in range(N):
+    C.append(list(map(int, sys.stdin.readline().split())))
+
+C = sorted(C, key = lambda x: (x[0], x[1]))
+
+for i in range(N):
+    if len(h) != 0 and h[0] <= C[i][0]:
+        heapq.heappop(h)
+    heapq.heappush(h, C[i][1])
+
+print(len(h))
